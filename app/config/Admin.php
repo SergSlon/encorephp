ed92d5c081632b14b8481b162b555c5b0f93eb86
Chris Harvey <chris@chrisnharvey.com>
2012-11-14 11:05:17 +0000

    Admin will be a seperate package

--- app/config/Admin.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYS_PATH') OR exit('No direct script access');
-
-$config['enable_admin'] = TRUE;
-$config['column_map'] = [
-	'VARCHAR' => 'textbox'
-];
--- app/config/Config.php
+++ app/config/Config.php
@@ -1,5 +1,4 @@
 <?php defined('SYS_PATH') OR exit('No direct script access');
 
-$config['enable_admin'] = TRUE;
 $config['helper_overload_prefix'] = 'MY_';
 $config['compress'] = FALSE;
